111f71fe391b61c2079d13bcdc53c71d2161e313
masomj <masonjayjones@gmail.com>
2022-07-15 21:40:34 +0100

    Added autofill current time when clicking the start and end time fields

--- mileagetracker/static/script.js
+++ mileagetracker/static/script.js
@@ -1,9 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     let nav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(nav);
-  });
+  M.Sidenav.init(nav);
+});
 
-  document.addEventListener('DOMContentLoaded', function() {
-    let records= document.querySelectorAll('.collapsible');
-    M.Collapsible.init(records);
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  let records= document.querySelectorAll('.collapsible');
+  M.Collapsible.init(records);
+});
+
+
+function fillTime(evnt){
+  const now = new Date()
+  ele = evnt.target
+  ele.value = ("0" + now.getHours()).slice(-2) + ":" + ("0" + now.getMinutes()).slice(-2);
+}
+
+
+let startTime =document.getElementById("start_time");
+let endTime = document.getElementById("end_time");
+
+
+endTime.addEventListener('click',fillTime);
+startTime.addEventListener('click',fillTime);
