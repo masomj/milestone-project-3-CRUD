2074a1308359d00fb4d18d4964d2f119950cd1f9
masomj <masonjayjones@gmail.com>
2022-07-16 13:37:50 +0100

    Updated to only use heroku DB

--- mileagetracker/__init__.py
+++ mileagetracker/__init__.py
@@ -10,18 +10,8 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-
-if os.environ.get("DEVELOPMENT") == "True":
-
-    pg_user = "postgres"
-    pg_pwd = "Mrdarcy2012??"
-    pg_port = "5432"
-    app.config["SQLALCHEMY_DATABASE_URI"] ="postgresql://{username}:{password}@localhost:{port}/mileagetracker".format(username=pg_user, password=pg_pwd, port=pg_port)
-else:
-    uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://",1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+print(os.environ.get("DATABASE_URL"))
 
 db = SQLAlchemy(app)
 
